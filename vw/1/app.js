244eee399bbdb648cdd8e4f9fa78d47d48777478
hyndf05 <79502403+hyndf05@users.noreply.github.com>
2022-06-14 09:00:33 +0000

    v1.0.34 - Words Added - Error Fixed - Code Modified

--- vw/1/app.js
+++ vw/1/app.js
@@ -5,6 +5,14 @@ $(window).bind('load', function(){
   },t)
 })
 
-$('.site-as span').click(function(){
+$('.site-as span.head').click(function(){
   location.href = '/';
 })
+
+$('.ad-inner .b span').click(function(){
+  window.open('https://chatanonymously-kr.hyndf05projects.repl.co/');
+})
+
+var backto =  document.referrer;
+
+console.log('%cbackground images by picsum.photos', 'padding: 3px 6px;font-size:7px;color:#26466c;');
--- vw/1/plastic.js
+++ vw/1/plastic.js
@@ -1,7 +1,10 @@
-var v = '1.0.23';
+var v = '1.0.34';
 console.log(`%c${v}`,'font-size:19px;padding:7px;');
+$('.version').text('');
 
-var visual = ['안빈낙도', '안분지족', '군계일학', '자승자강', '자강불식', '선공후사', '일념통천', '유비무환', '언행일치', '고진감래', '각인각색', '각주구검', '간담상조', '갑론을박', '대명천지', '도원결의', '목불인견', '명명백백', '부창부사', '빙탄지간', '구밀복검', '막역지우', '난신적자', '백골난망', '부화내동', '용두사미', '조강지처', '천고마비', '표리부동', '함흥차사', '죽마고우', '백면서생', '환골탈태', '노심초사', '다다익선', '역지사지', '일석이조', '주객전도', '마이동풍'];
+$('.head').text('Verify With Words');
+
+var visual = ['감기', '기생', '생활', '발생', '생명', '명암', '암석', '석탄', '탄소', '소금', '금성', '성공', '공부', '부채', '채무', '무기', '기괴', '괴물', '물리', '리본', '본드', '드릴', '후기', '리뷰', '행동', '동물', '식물', '자아', '아기', '기타', '타조', '조수', '수질', '질소', '소동', '동사', '사물', '물가', '가치', '치욕', '욕망', '망치', '치약', '약물', '물건', '원장', '운송', '코딩', '코팅', '고무', '당근', '오이', '사과', '가지', '김치', '자두', '치킨', '피자', '음료', '키위', '영상', '사진', '기능', '화장', '독감', '세균', '마약', '버스', '택시', '항공'];
 var mVisual = Math.random() * visual.length | 0;
 var rVisual = visual[mVisual];
 $('.section .text > span:nth-child(1)').text(rVisual);
@@ -14,7 +17,7 @@ function go() {
   var checkV = check.val();
   var checkL = check.val().length;
   if (word == checkV && checkL > 0) {
-    location.href = '/request/pass/index.html?from=ko1';
+    location.href = backto;
   } else if (word != checkV && checkL > 0) {
     var mVisual = Math.random() * visual.length | 0;
     var rVisual = visual[mVisual];
